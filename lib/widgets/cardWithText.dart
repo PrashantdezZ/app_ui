b3e8d720d3d9f3566615bab732f3b8566a710dd0
PrashantdezZ <prashantdezz@gmail.com>
2022-12-14 22:20:41 +0545

    gesture detector added

--- lib/widgets/cardWithText.dart
+++ lib/widgets/cardWithText.dart
@@ -9,67 +9,70 @@ class CardWithText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(5),
-      
-      width: 168,
-      decoration: BoxDecoration(
-        color: Palette.cardfillColor,
-        borderRadius: BorderRadius.circular(6),
-        boxShadow: [
-          BoxShadow(
-              offset: Offset(1, 1),
-              spreadRadius: 0,
-              blurRadius: 3,
-              color: Color.fromRGBO(87, 87, 87, 1).withOpacity(0.25),
-          ),
-          BoxShadow(
-              offset: Offset(1, 1),
-              spreadRadius: 0,
-              blurRadius: 3,
-              color: Color.fromRGBO(87, 87, 87, 1).withOpacity(0.25),
-          ),
-        ]
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-
-        children: [
-          Container(
-            height: 118,
-            width: 168,
-            decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.only(topLeft: Radius.circular(6),topRight: Radius.circular(6)),
-                
-                      image:  DecorationImage(
-                        fit: BoxFit.fill,
-                        image: AssetImage('assets/images/card.png')
-                      )
-                    ),
-          ),
-          // Text('google'),
-
-          
-          Container(
-            height: 22,
-            width: 168,
-            padding: EdgeInsets.only(top:8,left:8),
-            decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.only(bottomLeft: Radius.circular(6),bottomRight: Radius.circular(6)),
-
-                      
-                    ),
-            child: Text('Heart',
-            style: GoogleFonts.inter(
-              fontSize: 14,
-              fontWeight: FontWeight.w500,
-              color: Palette.black
+    return GestureDetector(
+      onTap: (){},
+      child: Container(
+        margin: EdgeInsets.all(5),
+        
+        width: 168,
+        decoration: BoxDecoration(
+          color: Palette.cardfillColor,
+          borderRadius: BorderRadius.circular(6),
+          boxShadow: [
+            BoxShadow(
+                offset: Offset(1, 1),
+                spreadRadius: 0,
+                blurRadius: 3,
+                color: Color.fromRGBO(87, 87, 87, 1).withOpacity(0.25),
             ),
-            
-            
+            BoxShadow(
+                offset: Offset(1, 1),
+                spreadRadius: 0,
+                blurRadius: 3,
+                color: Color.fromRGBO(87, 87, 87, 1).withOpacity(0.25),
             ),
-          )
-        ],
+          ]
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+    
+          children: [
+            Container(
+              height: 118,
+              width: 168,
+              decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.only(topLeft: Radius.circular(6),topRight: Radius.circular(6)),
+                  
+                        image:  DecorationImage(
+                          fit: BoxFit.fill,
+                          image: AssetImage('assets/images/card.png')
+                        )
+                      ),
+            ),
+            // Text('google'),
+    
+            
+            Container(
+              height: 22,
+              width: 168,
+              padding: EdgeInsets.only(top:8,left:8),
+              decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.only(bottomLeft: Radius.circular(6),bottomRight: Radius.circular(6)),
+    
+                        
+                      ),
+              child: Text('Heart',
+              style: GoogleFonts.inter(
+                fontSize: 14,
+                fontWeight: FontWeight.w500,
+                color: Palette.black
+              ),
+              
+              
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/widgets/groupOrder.dart
+++ lib/widgets/groupOrder.dart
@@ -9,71 +9,73 @@ class GroupOderButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-       height: 100,
-       width: 360,
-       child: Container(
-                   height: 99,
-                   width: 360,
-                   padding: EdgeInsets.all(27),
-                   decoration: BoxDecoration(
-                   color: Palette.white,
-                   boxShadow: [
-                     BoxShadow(
-                               offset: Offset(0, -4),
-                               spreadRadius: 0,
-                               blurRadius: 8,
-                               color: Color.fromRGBO(151, 151, 151, 1).withOpacity(0.29),
-                           )
-                   ]
-                 ),
-                 child: Container(
-                   height: 45,
-                   width: 320,
-                   padding: EdgeInsets.all(10),
-                   
-                   decoration: BoxDecoration(
-               color: Palette.darkPink,
-               borderRadius: BorderRadius.circular(8),
-         
-             ),
-             child: Row(
-               crossAxisAlignment: CrossAxisAlignment.center,
-               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-               children: [
-                 Container(
-                     padding: EdgeInsets.only(left: 8,top: 7),
-                     height: 26,
-                     width: 26,
+    return GestureDetector(
+      child: SizedBox(
+         height: 100,
+         width: 360,
+         child: Container(
+                     height: 99,
+                     width: 360,
+                     padding: EdgeInsets.all(27),
                      decoration: BoxDecoration(
-                       borderRadius: BorderRadius.circular(13),
-                       border: Border.all(color: Palette.white),
-                     ),
-                     child: Text('2',style: GoogleFonts.tajawal(
-                       fontWeight: FontWeight.w500,
-                       fontSize: 14,
-                       color: Palette.white
-                     ),)
-                     ),
-                   Text('View Your Order',
-                       style:GoogleFonts.tajawal(
-                         fontWeight: FontWeight.w700,
-                         fontSize: 18,
-                         color: Palette.white
-                       ) ,
+                     color: Palette.white,
+                     boxShadow: [
+                       BoxShadow(
+                                 offset: Offset(0, -4),
+                                 spreadRadius: 0,
+                                 blurRadius: 8,
+                                 color: Color.fromRGBO(151, 151, 151, 1).withOpacity(0.29),
+                             )
+                     ]
                    ),
-                   Text('\u{20B9}2700',
-                       style:GoogleFonts.tajawal(
-                         fontWeight: FontWeight.w700,
-                         fontSize: 18,
+                   child: Container(
+                     height: 45,
+                     width: 320,
+                     padding: EdgeInsets.all(10),
+                     
+                     decoration: BoxDecoration(
+                 color: Palette.darkPink,
+                 borderRadius: BorderRadius.circular(8),
+           
+               ),
+               child: Row(
+                 crossAxisAlignment: CrossAxisAlignment.center,
+                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                 children: [
+                   Container(
+                       padding: EdgeInsets.only(left: 8,top: 7),
+                       height: 26,
+                       width: 26,
+                       decoration: BoxDecoration(
+                         borderRadius: BorderRadius.circular(13),
+                         border: Border.all(color: Palette.white),
+                       ),
+                       child: Text('2',style: GoogleFonts.tajawal(
+                         fontWeight: FontWeight.w500,
+                         fontSize: 14,
                          color: Palette.white
-                       ) ,
-                   )
-               ],
-             ),
-                 ),
-                  ),
-                   );
+                       ),)
+                       ),
+                     Text('View Your Order',
+                         style:GoogleFonts.tajawal(
+                           fontWeight: FontWeight.w700,
+                           fontSize: 18,
+                           color: Palette.white
+                         ) ,
+                     ),
+                     Text('\u{20B9}2700',
+                         style:GoogleFonts.tajawal(
+                           fontWeight: FontWeight.w700,
+                           fontSize: 18,
+                           color: Palette.white
+                         ) ,
+                     )
+                 ],
+               ),
+                   ),
+                    ),
+                     ),
+    );
   }
 }
 
